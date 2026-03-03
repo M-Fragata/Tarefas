3f0dbbe018e375687c5545beb6031b4eae5d8726
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-03 17:58:56 -0300

    Adicionando o tratamento de erros no final do nosso app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import { routes } from "./routes"
+import { ErrorHandle } from "./middleware/ErrorHandle"
 
 const app = express()
 
+
 app.use(express.json())
+app.use(routes)
+
+app.use(ErrorHandle)
 
 export { app }
