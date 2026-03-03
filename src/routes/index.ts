2849ea61315457c11c6f3d8cecc2c3ece5d60982
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-03 18:52:04 -0300

    Adicionando as rotas de teams e tasks na rota principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { userRoutes } from "./user-routes";
+import { teamRoutes } from "./team-routes"
+import { taskRoutes } from "./task-routes"
 
 const routes = Router()
 
 routes.use("/users", userRoutes)
+routes.use("/teams", teamRoutes)
+routes.use("/tasks", taskRoutes)
 
 
 export { routes }
