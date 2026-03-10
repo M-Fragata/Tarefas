6785de700b6c13656832e427fb1f68b2e35a5977
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-10 20:58:43 -0300

    adicionando middlewares de authorization e authentication na rota de tasks

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { TaskController } from "../controller/Task-Controller"
 
+import { authenticated } from "../middleware/authenticated"
+import { authorizated } from "../middleware/authorizated"
+
 const taskRoutes = Router()
 const taskController = new TaskController
 
-taskRoutes.post("/", taskController.create)
-taskRoutes.get("/", taskController.index)
+taskRoutes.use(authenticated)
+
+taskRoutes.post("/", authorizated(["admin"]),taskController.create)
+taskRoutes.get("/", authorizated(["user","admin"]), taskController.index)
 
 export { taskRoutes }
