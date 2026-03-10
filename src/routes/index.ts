e6e3fa9910e7d4a2faa1819f2d8f13e330cd6f9e
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-10 20:58:17 -0300

    adicionando a rota de members

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { userRoutes } from "./user-routes";
 import { teamRoutes } from "./team-routes"
 import { taskRoutes } from "./task-routes"
+import { memberRoutes } from "./member-routes"
 
 const routes = Router()
 
 routes.use("/users", userRoutes)
 routes.use("/teams", teamRoutes)
 routes.use("/tasks", taskRoutes)
+routes.use("/members", memberRoutes)
 
 
 export { routes }
