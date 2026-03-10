5519b1ab9412f4858ddf944cd7190570d768a5e9
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-10 20:59:03 -0300

    adicionando middlewares de authorization e authentication na rota de teams, alem de criar a rota de delete e put

--- src/routes/team-routes.ts
+++ src/routes/team-routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { TeamController } from "../controller/Team-Controller"
 
+import { authenticated } from "../middleware/authenticated"
+import { authorizated } from "../middleware/authorizated"
+
 const teamRoutes = Router()
 const teamController = new TeamController
 
+teamRoutes.use(authenticated, authorizated(["admin"]))
+
 teamRoutes.post("/", teamController.create)
 teamRoutes.get("/", teamController.index)
+teamRoutes.delete("/:id", teamController.remove)
+teamRoutes.put("/:id", teamController.update)
 
 export { teamRoutes }
