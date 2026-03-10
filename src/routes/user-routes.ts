236e60fdaaee727c97e5e73fb3437cc2ab0898d6
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-10 20:59:32 -0300

    adicionando middlewares de authorization e authentication nas rota de editar e deletar usuário e criando rota de login

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import { UserController } from "../controller/User-Controller"
 
+import { authenticated } from "../middleware/authenticated";
+import { authorizated } from "../middleware/authorizated";
+
 const userRoutes = Router()
 const userController = new UserController
 
 userRoutes.get("/", userController.index)
 userRoutes.post("/", userController.create)
+userRoutes.post("/login", userController.login)
+
+userRoutes.use(authenticated, authorizated(["admin"]))
+
 userRoutes.put("/:id", userController.put)
 userRoutes.delete("/:id", userController.remove)
+
 
 export { userRoutes }
