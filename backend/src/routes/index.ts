92b41e81413dd66f292794b5d81c4c5a5178f655
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-05 11:41:55 -0300

    Update index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import { userRoutes } from "./user-routes";
 import { teamRoutes } from "./team-routes"
 import { taskRoutes } from "./task-routes"
 import { memberRoutes } from "./member-routes"
+import { ecidadeRoutes } from "./ecidade-routes"
 
 const routes = Router()
 
@@ -10,6 +11,7 @@ routes.use("/users", userRoutes)
 routes.use("/teams", teamRoutes)
 routes.use("/tasks", taskRoutes)
 routes.use("/members", memberRoutes)
+routes.use("/ecidade", ecidadeRoutes)
 
 
 export { routes }
