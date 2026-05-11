bf8f5475e8f9ce280844e1e173e402b0c3373bfd
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-11 08:10:41 -0300

    Ajuste no passo a passo do ecidade

--- backend/src/services/EcidadeService.ts
+++ backend/src/services/EcidadeService.ts
@@ -1,5 +1,6 @@
 import { chromium } from 'playwright';
 import { Task } from '../types/Task';
+import { Locator } from 'puppeteer';
 
 export async function EcidadeService(dados: Task) {
 
@@ -8,6 +9,8 @@ export async function EcidadeService(dados: Task) {
 
     const usuario = process.env.ECIDADE_USER
     const senha = process.env.ECIDADE_PASSWORD
+
+    const escola = dados.saida
 
     try {
         await page.goto('https://ecidade.marica.rj.gov.br/e-cidade/login.php');
@@ -25,18 +28,45 @@ export async function EcidadeService(dados: Task) {
         await page.click('.modulo_1100747')
 
         await page.click('#menu_id_3470')
-        
+
         await page.click('#menu_id_1100881')
 
         await page.click('#menu_id_1100883')
 
         await page.click('div[title="Configurações da Janela"]')
-        
-        await page.click('.select2-selection__rendered')
+        //problema aqui -----------------
 
-        await page.click('.select2-search__field')
+        // 3. Pequena pausa para garantir que os scripts do Select2 foram anexados ao DOM
+        await page.waitForTimeout(500);
 
-        console.log('Login no e-cidade realizado com sucesso!');
+        // 4. Tente o clique agora
+        await page.keyboard.press('Tab')
+        await page.keyboard.press('Space');
+
+        await page.waitForTimeout(500);
+
+        escola?.split("").map(async (e) => {
+            await page.keyboard.press(e)
+        })
+
+        await page.keyboard.press('Enter')
+
+        await page.keyboard.press('Tab')
+        await page.keyboard.press('Tab')
+        await page.keyboard.press('Tab')
+        await page.keyboard.press('Space');
+
+        //erro aqui, tentando pesquisar
+
+        await page.waitForTimeout(1000);
+
+        // 2. Localiza o frame pelo nome na lista de frames ativos
+        const frame = page.frames().find(f => f.name() === 'iframe_a1');
+
+        if (!frame) return console.log('❌ Frame iframe_a1 não encontrado na página')
+
+            const botao = frame.locator('input#pesquisar');
+            await botao.dispatchEvent('click');
 
     } catch (error) {
         console.error('Erro ao tentar fazer login no e-cidade:', error);
