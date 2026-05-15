35b05e009408bf40bfaf2d2f6be5141fa604e3ab
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-15 17:20:37 -0300

    adicionando rota de GET com id no params

--- backend/src/routes/schools/schools-routes.ts
+++ backend/src/routes/schools/schools-routes.ts
@@ -6,5 +6,6 @@ const schoolsController = new SchoolsController()
 const schoolsRoutes = Router()
 
 schoolsRoutes.get("/unidades", schoolsController.unidades)
+schoolsRoutes.get("/:id", schoolsController.unidade)
 
 export { schoolsRoutes }
