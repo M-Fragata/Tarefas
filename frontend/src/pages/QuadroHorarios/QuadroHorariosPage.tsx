9068a918fe21aff9fb1a7f94aa30981fe5b87ede
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-15 17:20:58 -0300

    Ajustando para buscar dados do banco de dados

--- frontend/src/pages/QuadroHorarios/QuadroHorariosPage.tsx
+++ frontend/src/pages/QuadroHorarios/QuadroHorariosPage.tsx
@@ -5,25 +5,7 @@ const DIAS = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex'];
 const TEMPOS = [1, 2, 3, 4, 5, 6]; // 6 tempos por turno
 
 // Exemplo de dados de turmas (isso viria do seu banco via Escola -> Turma)
-const TurmasManha = [
-    { id: '21784', codigo: '411' },
-    { id: '21785', codigo: '412' },
-    { id: '21786', codigo: '413' },
-    { id: '21790', codigo: '511' },
-    { id: '21791', codigo: '512' },
-    { id: '21792', codigo: '513' },
-    { id: '21796', codigo: '611' },
-];
 
-const TurmasTarde = [
-    { id: '21787', codigo: '421' },
-    { id: '21788', codigo: '422' },
-    { id: '21789', codigo: '423' },
-    { id: '21797', codigo: '521' },
-    { id: '21799', codigo: '522' },
-    { id: '21750', codigo: '523' },
-    { id: '21749', codigo: '621' },
-];
 
 interface EscolaProps {
     id: number,
@@ -40,10 +22,21 @@ interface AulaData {
     cor: string;
 }
 
+interface Turma {
+    id: string;
+    escolaId: number;
+    codigoTurma: string;
+    idEcidade: number;
+    turno: string;
+}
+
 export function QuadroHorariosPage() {
 
     const [escolas, setEscolas] = useState<EscolaProps[]>([]);
     const [escolaSelecionada, setEscolaSelecionada] = useState<string>("");
+
+    const [TurmasManha, setTurmasManha] = useState<Turma[]>([]);
+    const [TurmasTarde, setTurmasTarde] = useState<Turma[]>([]);
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [slotAtivo, setSlotAtivo] = useState<string | null>(null);
@@ -83,6 +76,7 @@ export function QuadroHorariosPage() {
                 setEscolaSelecionada(data[0].nome)
             }
 
+
         } catch (error) {
             console.error("Erro ao buscar escolas:", error);
         }
@@ -105,7 +99,13 @@ export function QuadroHorariosPage() {
                     "Content-Type": "application/json",
                 },
             });
+
             const data = await response.json();
+
+            await data.turmas.map((turma: Turma) => {
+                if (turma.turno === "MANHA" || turma.turno === "INTEGRAL") setTurmasManha(prev => [...prev, turma])
+                if (turma.turno === "TARDE" || turma.turno === "INTEGRAL") setTurmasTarde(prev => [...prev, turma])
+            })
 
             console.log(data);
 
@@ -113,6 +113,15 @@ export function QuadroHorariosPage() {
             console.error("Erro ao buscar dados das escolas:", error);
         }
     }
+
+    useEffect(() => {
+
+        setTurmasManha([]);
+        setTurmasTarde([]);
+
+        handleGetSchoolsData();
+
+    }, [escolaSelecionada]);
 
     function openModal(key: string) {
         setSlotAtivo(key);
@@ -190,10 +199,10 @@ export function QuadroHorariosPage() {
                                 <tr className="bg-gray-100">
                                     <th className="min-w-8 border border-gray-300 border-r-gray-100 sticky left-0 z-10 bg-gray-100"></th>
                                     <th className="min-w-6 border border-gray-300 sticky left-0 z-10 bg-gray-100"></th>
-                                    {TurmasManha.map((turma) => (
+                                    {TurmasManha.map((turma: Turma) => (
                                         <th key={turma.id} className="w-40 border border-gray-300 p-1 text-center">
-                                            <div className="text-xs font-bold text-gray-600">{turma.codigo}</div>
-                                            <div className="text-[10px] text-gray-400">{turma.id}</div>
+                                            <div className="text-xs font-bold text-gray-600">{turma.codigoTurma}</div>
+                                            <div className="text-[10px] text-gray-400">{turma.idEcidade}</div>
                                         </th>
                                     ))}
                                 </tr>
@@ -212,7 +221,7 @@ export function QuadroHorariosPage() {
                                                 </td>
 
 
-                                                {TurmasManha.map((turma) => {
+                                                {TurmasManha.map((turma: Turma) => {
                                                     const slotKey = `${turma.id}-${dia}-${tempo}`;
                                                     const aula = gradeHoraria[slotKey];
 
@@ -280,10 +289,10 @@ export function QuadroHorariosPage() {
                                 <tr className="bg-gray-100">
                                     <th className="min-w-8 border border-gray-300 border-r-gray-100 sticky left-0 z-10 bg-gray-100"></th>
                                     <th className="min-w-6 border border-gray-300 sticky left-0 z-10 bg-gray-100"></th>
-                                    {TurmasTarde.map((turma) => (
+                                    {TurmasTarde.map((turma: Turma) => (
                                         <th key={turma.id} className="w-40 border border-gray-300 p-1 text-center">
-                                            <div className="text-xs font-bold text-gray-600">{turma.codigo}</div>
-                                            <div className="text-[10px] text-gray-400">{turma.id}</div>
+                                            <div className="text-xs font-bold text-gray-600">{turma.codigoTurma}</div>
+                                            <div className="text-[10px] text-gray-400">{turma.idEcidade}</div>
                                         </th>
                                     ))}
                                 </tr>
