45ccdd6e215f289d757b932e616dca4df45b0187
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-05-28 10:59:34 -0300

    Nova tabela de cargos do Ecidade

---
+++ backend/prisma/migrations/20260528134932_add_cargos_ecidade/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Cargos_ecidade" (
+    "id" TEXT NOT NULL,
+    "value" INTEGER NOT NULL,
+    "cargo" TEXT NOT NULL,
+
+    CONSTRAINT "Cargos_ecidade_pkey" PRIMARY KEY ("id")
+);
